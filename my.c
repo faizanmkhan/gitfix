b58b6678bd4993658ddf0d679aef619b0aeac01e
Lenovo WSLubuntu <systemd@wslubuntu.fr>
2025-05-31 17:13:09 +0200

    without commit push doesn't work

--- my.c
+++ my.c
@@ -1,1 +1,6 @@
+Generating the ssh key using ssh-keygen 
+adding it to the ssh section of git from setting
+setting up the git config --global user name and email.
+doing a git commit -m "message" also gives me the further instruction
+git push -u origin global
 this is my git file
